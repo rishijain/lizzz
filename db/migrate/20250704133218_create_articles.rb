14174c73368029583454f8a39497cb5ca4f187f9
rishijain <jainrishi.37@gmail.com>
2025-07-04 19:03:59 +0530

    added article and installed neighbor

---
+++ db/migrate/20250704133218_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.text :summary
+      t.timestamps
+    end
+  end
+end
